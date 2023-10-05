a6d258e85ac166e8a055059da32f759685b49c4a
suppu19 <supraja.angirekula@gmail.com>
2023-10-05 12:09:33 +0530

    gg

--- provider.tf
+++ provider.tf
@@ -12,3 +12,9 @@ provider "aws" {
   region = "ap-south-1"
   
 }
+
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = aws_s3_terraformfile
+  key    = ".terraform.lock.hcl"
+  source = "C:\Users\Vamsikrishna\Desktop\proj\terraform-vpc"  # Local path to your TF file
+}
